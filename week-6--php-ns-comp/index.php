ac93742cf1702315ae8b9e3918285ddcb2a5e792
Nik Osvalds <nosvalds@gmail.com>
2020-05-06 16:54:36 +0100

    start hello

--- week-6--php-ns-comp/index.php
+++ week-6--php-ns-comp/index.php
@@ -3,3 +3,12 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+echo "\nQuestion 1:\n";
+
+use App\Hello;
+
+$sayHi = new Hello();
+
+dump($sayHi->hello("Orb")); // "Hello Orb"
+dump($sayHi->hello("Horse")); // "Hello Horse"
