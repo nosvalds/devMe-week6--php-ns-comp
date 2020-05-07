3d12d31fae0c5dd24650d854f5483aded016ff56
Nik Osvalds <nosvalds@gmail.com>
2020-05-07 09:59:55 +0100

    Book 04 complete

---
+++ week-6--php-ns-comp/app/Library/Book.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace App\Library;
+
+class Book 
+{
+    // properties
+    private $title;
+    private $totalPages;
+    private $pagesRead = 1;
+
+    // constructor
+    public function __construct(string $title, int $totalPages)
+    {
+        $this->title = $title;
+        $this->totalPages = $totalPages;
+    }
+
+    // methods
+
+    // read a number of pages
+    public function read(int $pagesRead) : Book
+    {
+        $this->pagesRead += $pagesRead;
+        return $this;
+    }
+
+    // what page am I on?
+    public function currentPage() : int
+    {
+        return $this->pagesRead;
+    }
+}
