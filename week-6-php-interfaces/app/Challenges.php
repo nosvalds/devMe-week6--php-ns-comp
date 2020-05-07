82ca5f9aaf41002ddf8663c4e4c39105042a3cf2
Nik Osvalds <nosvalds@gmail.com>
2020-05-07 16:55:53 +0100

    Square complete

--- week-6-php-interfaces/app/Challenges.php
+++ week-6-php-interfaces/app/Challenges.php
@@ -25,14 +25,14 @@ class Challenges
 
         // create new shapes
         $square = new Shapes\Square(4);
-        $circle = new Shapes\Circle(4);
-        $rectangle = new Shapes\Rectangle(4, 5);
+        //$circle = new Shapes\Circle(4);
+        //$rectangle = new Shapes\Rectangle(4, 5);
 
         // log the areas of each
         dump(
             $square->area(), // 16
-            $circle->area(), // 50.265482457437
-            $rectangle->area() // 20
+            // $circle->area(), // 50.265482457437
+            // $rectangle->area() // 20
         );
     }
 
