ac128f3d777fb5366f3b9a9f61c43dd5bdb4584f
Nik Osvalds <nosvalds@gmail.com>
2020-05-07 10:42:15 +0100

    constructor and addShelf done

--- week-6--php-ns-comp/app/Library/Library.php
+++ week-6--php-ns-comp/app/Library/Library.php
@@ -4,5 +4,27 @@ namespace App\Library;
 
 class Library
 {
+    // properties
+    private $shelves;
 
+    // constructor
+    public function __construct()
+    {
+        $this->shelves = collect();
+
+    }
+
+    // methods
+
+    // add a shelf to the library
+    public function addShelf(Shelf $shelf)
+    {
+        $this->shelves->push($shelf);
+    }
+
+    // display all the titles in the library
+    public function titles()
+    {
+        return $this; // map titles() on each shelf and concat?
+    }
 }
