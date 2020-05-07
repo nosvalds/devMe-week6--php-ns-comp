1e1dfbb5d2356be8706e9c277ccab6b512fdb67a
Nik Osvalds <nosvalds@gmail.com>
2020-05-07 09:51:12 +0100

    Question 3 complete

--- week-6--php-ns-comp/app/Stuff/Things/Potato.php
+++ week-6--php-ns-comp/app/Stuff/Things/Potato.php
@@ -4,5 +4,21 @@ namespace App\Stuff\Things;
 
 class Potato
 {
-    
+    // properties
+    private $waterCount;
+
+    // methods
+
+    // water the potato
+    public function water() : Potato
+    {
+        $this->waterCount += 1;
+        return $this;
+    }
+
+    // check if it's grown
+    public function hasGrown() : bool
+    {
+        return $this->waterCount >= 5;
+    }
 }
