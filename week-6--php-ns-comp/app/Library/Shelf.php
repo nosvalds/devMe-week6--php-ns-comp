7231facfd68e326104414c5648e295e084ff3b55
Nik Osvalds <nosvalds@gmail.com>
2020-05-07 10:10:23 +0100

    add illuminate/support

--- week-6--php-ns-comp/app/Library/Shelf.php
+++ week-6--php-ns-comp/app/Library/Shelf.php
@@ -4,5 +4,15 @@ namespace App\Library;
 
 class Shelf
 {
-    
+    // properties
+    private $books = [];
+
+    // methods
+
+    // add Book to shelf
+    public function addBook(Book $book) : Shelf
+    {
+        $this->books[] = $book;
+        return $this;
+    }
 }
