856b74f08a8f881d9dcbb932639733be20ab1583
Nik Osvalds <nosvalds@gmail.com>
2020-05-08 12:28:05 +0100

    spoon class

---
+++ week-6--php-inheritance/app/Cutlery/Spoon.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Spoon;
+
+class Spoon
+{
+    // properties
+    private $scoops = 0;
+
+    // methods
+
+    // scoop
+    public function scoop() : Spoon    
+    {
+        $this->scoops += 1;
+        return $this;
+    }
+
+    // how many scoops?
+    public function howManyScoops() : int
+    {
+        return $this->scoops;
+    }
+}
