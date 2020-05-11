cb611d50948987772f58945e6c6557ab2cfc85e1
Nik Osvalds <nosvalds@gmail.com>
2020-05-11 16:26:36 +0100

    uber tricksy finished

---
+++ week-6--php-ns-comp/__DIR__/77/774fcad02a315c12583b66d920c8b570e0547e7fe28c1c95fedbb9da6b66f37e.php
@@ -0,0 +1,79 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* template.html */
+class __TwigTemplate_bb668ad2cc252041cd1dca6674ded8e02102be967473c636b193c09bb2fc761f extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!doctype html>
+<html lang=\"en\">
+    <head>
+        <meta charset=\"utf-8\">
+        <meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\">
+        <title>";
+        // line 6
+        echo twig_escape_filter($this->env, ($context["title"] ?? null), "html", null, true);
+        echo "</title>
+    </head>
+    <body>
+        <h1>";
+        // line 9
+        echo twig_escape_filter($this->env, ($context["title"] ?? null), "html", null, true);
+        echo "</h1>
+        <p>";
+        // line 10
+        echo twig_escape_filter($this->env, ($context["article"] ?? null), "html", null, true);
+        echo "</p>
+    </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "template.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  54 => 10,  50 => 9,  44 => 6,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "template.html", "/Users/nosvalds/Documents/CodingFellowship/Week6_Adv/week-6--php-ns-comp/app/Data/template.html");
+    }
+}
--- week-6--php-ns-comp/app/Data/Post.php
+++ week-6--php-ns-comp/app/Data/Post.php
@@ -2,20 +2,52 @@
 
 namespace App\Data;
 
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
 class Post
 {
+    private static $twig;
+
+    public static function setup()
+    {
+        if (!static::$twig) {
+            $loader = new FilesystemLoader(__DIR__ . "/templates/");
+            static::$twig = new Environment($loader, [
+                "cache" => __DIR__ . "/cache/",
+            ]);
+        }
+    }
+
     // properties
     private $title;
+    private $article;
 
     // constructor
     public function __construct(string $title)
     {
+        static::setup();
         $this->title = $title;
     }
 
     // setArticle
-    public function setArticle(string $article)
+    public function setArticle(string $article) : Post
     {
-        # code...
+        $this->article = $article;
+        return $this;
+    }
+
+    // render
+    public function render()
+    {
+        // $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . "/templates/");
+        // $twig = new \Twig\Environment($loader, [
+        //     'cache' => __DIR__ . "/cache/",
+        // ]);
+        
+        return static::$twig->render("template.html", [
+            'title' => $this->title, 
+            'article' => $this->article
+        ]);
     }
 }
