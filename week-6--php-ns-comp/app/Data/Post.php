bb8976b107d07e8bdaf8fbbdc1f2f1f177294333
Nik Osvalds <nosvalds@gmail.com>
2020-05-11 16:01:20 +0100

    set up uber-tricksy and install twig

---
+++ week-6--php-ns-comp/app/Data/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Data;
+
+class Post
+{
+    // properties
+    private $title;
+
+    // constructor
+    public function __construct(string $title)
+    {
+        $this->title = $title;
+    }
+
+    // setArticle
+    public function setArticle(string $article)
+    {
+        # code...
+    }
+}
